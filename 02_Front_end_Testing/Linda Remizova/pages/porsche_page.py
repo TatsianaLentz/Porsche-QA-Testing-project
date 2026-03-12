b55592976969a79ab175fb9769cf8f194cadef1f
Linda Remizova <lindaremizova@gmail.com>
2026-03-11 19:02:06 -0600

    Update Porsche tests

--- 02_Front_end_Testing/Linda Remizova/pages/porsche_page.py
+++ 02_Front_end_Testing/Linda Remizova/pages/porsche_page.py
@@ -1,8 +1,8 @@
 import time
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
-class PorschePage:
+class PorscheFirefoxPage:
     URL = "https://www.porsche.com/international/models/718/718-models/718-cayman/"
 
     def __init__(self, driver):
@@ -10,11 +10,22 @@ class PorschePage:
 
     def open(self):
         self.driver.get(self.URL)
+        time.sleep(6)
 
-    def accept_cookies_with_tab(self, tabs: int = 3, wait: float = 2.0):
-        actions = (Keys.TAB * tabs) + Keys.ENTER
-        self.driver.switch_to.active_element.send_keys(actions)
-        time.sleep(wait)
+    def accept_cookies(self):
+        # layer2 host
+        host = self.driver.find_element(By.CSS_SELECTOR, "uc-layer2")
+        shadow1 = host.shadow_root
 
-    def open_invalid_page(self):
-            self.driver.get("https://www.porsche.com/usa/models/718/718-models/INVALID-PAGE/")
+        # modal
+        modal = shadow1.find_element(By.CSS_SELECTOR, "uc-p-modal.modal")
+
+        # footer
+        footer = modal.find_element(By.CSS_SELECTOR, "uc-footer.footer")
+        shadow2 = footer.shadow_root
+
+        # accept button
+        accept = shadow2.find_element(By.CSS_SELECTOR, "uc-p-button.accept")
+        self.driver.execute_script("arguments[0].click();", accept)
+
+        time.sleep(2)
